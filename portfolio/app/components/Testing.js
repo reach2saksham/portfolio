5eb42535354cea130fe16268fe4a5db7aaa74615
Saksham Jain <saksham_j@ar.iitr.ac.in>
2024-12-30 01:13:14 +0530

    Projects section prototype completed

--- portfolio/app/components/Testing.js
+++ portfolio/app/components/Testing.js
@@ -1,75 +1,103 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 
-const stack1Order = [
-    'figma.svg',
-    'css3.svg',
-    'js.svg',
-    'c++.svg',
-    'reactjs.svg',
-    'Frame15.svg',
-    'bootstrap4.svg',
-    'mongodb.svg',
-    'mysql.svg',
-    'nodejs.svg'
-  ];
-  
-  const stack2Order = [
-    'figma.svg',
-    'adobe.svg',
-    'ai.svg',
-    'canva.svg',
-    'linux.svg',
-    'git.svg',
-    'github.svg',
-    'miro.svg'
-  ];
-  
+const Projects = () => {
+    return (
+        <div className='projects flex gap-2 items-center justify-between max-w-full container pt-4 mx-auto xl:px-36 lg:px-14 sm:px-4'>
 
-const Testing = () => {
-  return (
-    <div>
-      <div className="col-span-3 order-10 lg:order-10 sm:col-span-2 md:col-span-2 lg:col-span-6 row-span-2 lg:row-span-3 bg-[#0F0F0F] border border-[#363636]/20 rounded-3xl flex flex-col sm:flex-row justify-center items-center group">
-  <div className="w-full sm:w-1/4 px-2 pb-2 sm:pb-0 h-auto sm:h-full flex flex-col justify-end text-xl sm:text-3xl pt-2 sm:pt-3 text-[#B8B8B8] font-bold text-center sm:text-left">
-    STACK
-  </div>
-  <div className="w-full sm:w-3/4 h-auto sm:h-full flex flex-col justify-around gap-3">
-    <div className="bg-[#313034]/[0.7] rounded-xl sm:rounded-r-none h-auto sm:h-2/6 flex gap-2 px-2 sm:px-3 py-2 flex-wrap">
-      {stack1Order.map((icon, index) => (
-        <Image
-          key={index}
-          className="w-10 h-10 sm:w-12 sm:h-12 grayscale group-hover:grayscale-0 transition duration-300"
-          src={`/STACK1/${icon}`}
-          width={50}
-          height={50}
-          alt={icon.split('.')[0]}
-        />
-      ))}
-    </div>
-    <div className="bg-[#313034]/[0.7] rounded-xl sm:rounded-r-none h-auto sm:h-2/6 flex gap-2 px-2 sm:px-3 py-2 flex-wrap">
-      <Image
-        className="w-8 h-8 sm:w-10 sm:h-10 grayscale group-hover:grayscale-0 transition duration-300"
-        src={`/STACK2/framer.svg`}
-        width={34}
-        height={50}
-        alt="framer"
-      />
-      {stack2Order.map((icon, index) => (
-        <Image
-          key={index}
-          className="w-10 h-10 sm:w-12 sm:h-12 grayscale group-hover:grayscale-0 transition duration-300"
-          src={`/STACK2/${icon}`}
-          width={50}
-          height={50}
-          alt={icon.split('.')[0]}
-        />
-      ))}
-    </div>
-  </div>
-</div>
+                <div className='p-4'>
+                    <div className='projecthead text-7xl text-center mb-4'>DESIGN</div>
 
-    </div>
-  )
-}
+                    <div className='bg-[#131313] w-1/3 rounded-3xl relative group'>
+                {/* Image Section */}
+                <div className='relative'>
+                    <Image
+                        className='w-full h-full object-cover rounded-3xl'
+                        src='/bottles.png'
+                        width={350}
+                        height={380}
+                        alt='Saksham Jain'
+                        priority
+                        unoptimized={true}
+                    />
+                    {/* Black Overlay on Hover */}
+                    <div className='absolute inset-0 bg-black opacity-0 group-hover:opacity-70 transition-opacity rounded-3xl'></div>
 
-export default Testing
+                    {/* Top Overlay - Flex with justify-between */}
+                    <div className='absolute top-3 left-3 right-3 flex justify-between items-center p-2'>
+                        <div className='flex gap-2'>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:bg-[#1A1A1A] group-hover:text-white group-hover:shadow-lg'>
+                                Internship
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:bg-[#1A1A1A] group-hover:text-white group-hover:shadow-lg'>
+                                July 2024 - Sept 2024
+                            </span>
+                        </div>
+                        <span className='tags bg-[#E6E6E6] text-[#1A1A1A] text-sm font-semibold rounded-full w-5 h-5 flex justify-center items-center'>
+                            <ArrowUpRightIcon className="h-3 w-3" />
+                        </span>
+                    </div>
+
+                    {/* Bottom Overlay - Flex column with gap-2 */}
+                    <div className='hidden group-hover:flex absolute bottom-3 left-3 right-3 flex-col gap-2 p-2'>
+                        <div className='flex gap-2'>
+                            <span className='tags px-2 py-[2px] text-[10px] rounded-2xl bg-[#1A1A1A] text-white group-hover:shadow-lg'>
+                                Role
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                UI/UX Designer
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                Product Manager
+                            </span>
+                        </div>
+                        <div className='flex gap-2'>
+                            <span className='tags px-2 py-[2px] text-[10px] rounded-2xl bg-[#1A1A1A] text-white group-hover:shadow-lg'>
+                                Domain
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                UI/UX Designer
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                Product Manager
+                            </span>
+                        </div>
+                        <div className='flex gap-2'>
+                            <span className='tags px-2 py-[2px] text-[10px] rounded-2xl bg-[#1A1A1A] text-white group-hover:shadow-lg'>
+                                Impact
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                Increased User Retention
+                            </span>
+                            <span className='tags bg-[#E6E6E6] text-[#1A1A1A] px-2 py-[2px] text-[10px] rounded-2xl group-hover:shadow-lg'>
+                                Streamlined Workflows
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+                    
+                    <div className='font-semibold text-lg'>CLIENT SIDE DASHBOARD</div>
+                    <div className='text-xs'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptatem similique dolore necessitatibus iste commodi corrupti magnam a tenetur, laboriosam inventore nam, ea cum molestiae eaque.
+                    </div>
+                </div>
+            {/* Design Project */}
+            
+            <div className='bg-[#131313] w-1/3 text-center rounded-3xl'>
+                <div className='projecthead text-7xl'>DEVELOPMENT</div>
+                <div></div>
+            </div>
+
+            <div className='bg-[#131313] w-1/3 text-center rounded-3xl'>
+                <div className='projecthead text-7xl'>PRODUCT</div>
+                <div></div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Projects;
