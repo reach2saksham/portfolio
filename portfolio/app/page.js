4e64706b78777a191fb6d4f78e448306e9210053
Saksham Jain <saksham_j@ar.iitr.ac.in>
2025-06-24 23:34:31 +0530

    Fixes 12

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 
 export const metadata = {
   title: "Saksham Jain",
-  description: "Hi, I am a student from IIT Roorkee. I'm selectively skilled in product design and management with strong focus on developing high quality & impactful digital experiences.",
+  description: "Hi, I'm a selectively skilled product designer from IIT Roorkee with strong focus on developing high quality & impactful digital experiences.",
   keywords: [
     "Saksham", "Jain", "Saksham Jain", "IIT", "Roorkee", "IITR", "IIT Roorkee",
     "Design", "graphics", "visuals", "designer from IIT Roorkee",
@@ -20,14 +20,28 @@ export const metadata = {
     siteName: "Saksham Jain Portfolio",
     images: [
       {
-        src: "/images/3/1.avif",
+        src: "https://www.sakshamjain.in/images/3/1.avif",
         width: 1200,
         height: 630,
+        alt: "Saksham Jain Portfolio Cover"
       },
     ],
     locale: "en_IN",
     type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Saksham Jain",
+    description: "Hi, I'm a selectively skilled product designer from IIT Roorkee with strong focus on developing high quality & impactful digital experiences.",
+    images: ["https://www.sakshamjain.in/images/3/1.avif"]
+  },
+  alternates: {
+    canonical: "https://www.sakshamjain.in"
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
 };
 
 export default function Page() {
