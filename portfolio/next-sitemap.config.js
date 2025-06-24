804863450fb38638d5a851a76724a29f1611d0ee
Saksham Jain <saksham_j@ar.iitr.ac.in>
2025-06-24 19:51:59 +0530

    Fixes 10

---
+++ portfolio/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.sakshamjain.in',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false, // optional: disables index sitemap if not needed
+};
